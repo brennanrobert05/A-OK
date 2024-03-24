d8f80e15385d7ea628e88f6010df9f48f0defb5a
brennanrobert05 <66694910+brennanrobert05@users.noreply.github.com>
2024-03-24 18:38:14 +0000

    Create josh_murphy.dart

---
+++ josh_murphy.dart
@@ -0,0 +1,16 @@
+// josh_murphy.dart
+import 'package:flutter/material.dart';
+
+class JoshMurphyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Josh Murphy Page"),
+      ),
+      body: Center(
+        child: Text("Content for Josh Murphy's Page"),
+      ),
+    );
+  }
+}
