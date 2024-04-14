fc95ecef3488d509d75dbae07f1b152eecb07c85
brennanrobert05 <66694910+brennanrobert05@users.noreply.github.com>
2024-04-14 12:35:36 +0100

    Update login_or_register_page.dart

--- lib/pages/login_or_register_page.dart
+++ lib/pages/login_or_register_page.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_page.dart';
+import 'register_page.dart';
 
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  // initially show login page
+  bool showLoginPage = true;
+
+  // toggle between login + register page
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: togglePages,
+      );
+    } else {
+      return RegisterPage(
+        onTap: togglePages,
+      );
+    }
+  }
+}
